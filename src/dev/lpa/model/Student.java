5da3add80569e56342ac4659359b947baba5a6f5
adamd <adam.d.hinton@gmail.com>
2024-03-09 00:09:21 -0500

    flesh out Student method with randomized name, yearStarted and courses

--- src/dev/lpa/model/Student.java
+++ src/dev/lpa/model/Student.java
@@ -8,9 +8,27 @@ public class Student {
     private String course;
     private int yearStarted;
 
+    // want Students to access
     protected static Random random = new Random();
 
     private static String[] firstNames = {"Ann", "Bill", "Cathy", "John", "Tim"};
 
     private static String[] courses = {"C++", "Java", "Python"};
+
+    public Student(){
+        // selects random capital letter
+        int lastNameIndex = random.nextInt(65, 91);
+        name = firstNames[random.nextInt(5)] + " " + (char) lastNameIndex;
+        course = courses[random.nextInt(3)];
+        yearStarted = random.nextInt(2018, 2023);
+    }
+
+    @Override
+    public String toString() {
+        return "%-15s %-15s %d".formatted(name, course, yearStarted);
+    }
+
+    public int getYearStarted() {
+        return yearStarted;
+    }
 }
