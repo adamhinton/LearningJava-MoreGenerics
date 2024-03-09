65600e0a44c4841b4fe7e0204f8df56aadd03c8d
adamd <adam.d.hinton@gmail.com>
2024-03-09 00:13:54 -0500

    test Student in main

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,7 +1,26 @@
 package dev.lpa;
 
+import dev.lpa.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        int studentCount = 10;
+        List<Student> students = new ArrayList<>();
+        for(int i=0; i<studentCount; i++){
+            students.add(new Student());
+        }
+        printList(students);
+
     }
+
+    public static void printList(List<Student> students){
+        for (var student : students){
+            System.out.println(student);
+        }
+    }
+
 }
